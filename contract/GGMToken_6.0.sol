81b00f0ec2fb805c5ffd0cb96f00646f9d9348fe
dannyly96 <danny_ly96@hotmail.com>
2021-05-06 21:19:34 +1000

    a

--- contract/GGMToken_6.0.sol
+++ contract/GGMToken_6.0.sol
@@ -1,11 +1,11 @@
 pragma solidity 0.6.6;
 
 // ----------------------------------------------------------------------------
-// 'GGMToken' token contract
+// 'Sheesh' token contract
 //
-// Deployed to : 0xFEB02D9383C49A8373F88e82EbCecB553c1837bf
-// Symbol      : GGM
-// Name        : GGMToken
+// Deployed to : 0xA7D2239aC0DbBE64a4F104AF727c324586AE6056
+// Symbol      : SHS
+// Name        : Sheesh
 // Total supply: 100000000
 // Decimals    : 18
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract GGMToken is ERC20Interface, Owned, SafeMath {
+contract Sheesh is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract GGMToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "CCM";
-        name = "CCMToken";
-        decimals = 0;
+        symbol = "SHS";
+        name = "Sheesh";
+        decimals = 18;
         _totalSupply = 100000000;
-        balances[0x4B6408E5De3D348C0cd571E67Aa2dDBDe563AD58] = _totalSupply;
-        emit Transfer(address(0), 0x4B6408E5De3D348C0cd571E67Aa2dDBDe563AD58, _totalSupply);
+        balances[0xA7D2239aC0DbBE64a4F104AF727c324586AE6056] = _totalSupply;
+        emit Transfer(address(0), 0xA7D2239aC0DbBE64a4F104AF727c324586AE6056, _totalSupply);
     }
 
 
